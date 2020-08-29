efd0b408444c83246fb62e86c3ba7a696adb643d
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 01:26:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,4 +11,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
   
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   Object[key] = value
+  return(object)
 }  
