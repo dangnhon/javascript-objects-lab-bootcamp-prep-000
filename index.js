e87080dad8b70f5e676d92ec8dc176f5adaeee7c
Nhon Dang <dang.nhon12345@yahoo.com>
2020-08-29 01:32:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   var clone = Object.assign({}, object)
-  delete clone.key 
+  delete clone[key] 
   return(clone)
 }
